4c058ddc6e9f4ad5a9a497e417ee33612d9a8055
zhuzhenfeng.code <zhuzhenfeng.code@bytedance.com>
2021-09-20 22:22:24 +0800

    解释器

--- intepretor.ts
+++ intepretor.ts
@@ -12,7 +12,8 @@ import {
     Variable,
     VariableDecl,
 } from './ast'
-import { VarSymbol } from './symbol'
+import { Op } from './scanner'
+import { VarSymbol, Symbol } from './symbol'
 
 export class Intepretor extends AstVisitor {
     callStack: StackFrame[] = []
@@ -32,7 +33,7 @@ export class Intepretor extends AstVisitor {
         this.currentFrame = frame
     }
 
-    private popFrame(frame: StackFrame) {
+    private popFrame() {
         if (this.callStack.length > 1) {
             let frame = this.callStack[this.callStack.length - 2]
             this.callStack.pop()
@@ -103,35 +104,246 @@ export class Intepretor extends AstVisitor {
         }
     }
 
-    visitForStatement(forStmt: ForStatement): any {}
+    visitForStatement(forStmt: ForStatement): any {
+        // 执行init
+        if (forStmt.init != null) {
+            this.visit(forStmt.init)
+        }
 
-    visitFunctionCall(functionCall: FunctionCall): any {}
+        // 计算循环结束的条件
+        let notTerminate =
+            forStmt.condition == null ? true : this.visit(forStmt.condition)
+        while (notTerminate) {
+            // 执行循环
+            let retVal = this.visit(forStmt.stmt)
+            // 处理循环中的return语句
+            if (
+                typeof retVal == 'object' &&
+                ReturnValue.isReturnValue(retVal)
+            ) {
+                return retVal
+            }
 
-    private println(args: Expression[]): any {}
+            // 执行增量表达式
+            if (forStmt.increment != null) {
+                this.visit(forStmt.increment)
+            }
 
-    private tick(): number {}
+            notTerminate =
+                forStmt.condition == null ? true : this.visit(forStmt.condition)
+        }
+    }
 
-    private integer_to_string(args: Expression[]): string {}
+    /**
+     * 运行函数调用
+     * 原理：根据函数定义，执行函数体
+     * @param functionCall
+     */
+    visitFunctionCall(functionCall: FunctionCall): any {
+        if (functionCall.name == 'println') {
+            return this.println(functionCall.arguments)
+        } else if (functionCall.name == 'tick') {
+            return this.tick
+        } else if (functionCall.name == 'integer_to_string') {
+            return this.integer_to_string(functionCall.arguments)
+        }
 
-    visitVariableDecl(variableDecl: VariableDecl): any {}
+        if (functionCall.sym != null) {
+            // 清空返回值
+            this.currentFrame.retVal = undefined
 
-    visitVariable(v: Variable): any {}
+            // 创建新的栈帧
+            let frame = new StackFrame()
 
-    private getVariableValue(sym: VarSymbol): any {}
+            // 计算参数值，并保存到新创建的栈帧中
+            let functionDecl = functionCall.sym.decl as FunctionDecl
+            if (functionDecl.callSignature.paramList != null) {
+                let params = functionDecl.callSignature.paramList.params
+                for (let i = 0; i < params.length; i++) {
+                    let variableDecl = params[i]
+                    let val = this.visit(functionCall.arguments[i])
+                    // 设置到新的frame
+                    frame.values.set(variableDecl.sym as Symbol, val)
+                }
+            }
 
-    private setVariableValue(sym: VarSymbol, value: any): any {}
+            // 把新栈帧入栈
+            this.pushFrame(frame)
+
+            // 执行函数
+            this.visit(functionDecl.body)
+
+            // 弹出当前栈帧
+            this.popFrame()
+
+            //函数返回值
+            return this.currentFrame.retVal
+        } else {
+            console.log(
+                'Runtime error, cannot find declaration of ' +
+                    functionCall.name +
+                    '.'
+            )
+            return
+        }
+    }
+
+    private println(args: Expression[]): any {
+        if (args.length > 0) {
+            let retVal = this.visit(args[0])
+            console.log(retVal)
+        } else {
+            console.log()
+        }
+        return 0
+    }
+
+    private tick(): number {
+        let date = new Date()
+        let value = Date.UTC(
+            date.getFullYear(),
+            date.getMonth(),
+            date.getDate(),
+            date.getHours(),
+            date.getMinutes(),
+            date.getSeconds(),
+            date.getMilliseconds()
+        )
+        return value
+    }
+
+    private integer_to_string(args: Expression[]): string {
+        if (args.length > 0) {
+            let arg = this.visit(args[0])
+            return args.toString()
+        }
+
+        return ''
+    }
+
+    visitVariableDecl(variableDecl: VariableDecl): any {
+        if (variableDecl.init != null) {
+            let v = this.visit(variableDecl.init)
+            this.setVariableValue(variableDecl.sym as VarSymbol, v)
+            return v
+        }
+    }
+
+    visitVariable(v: Variable): any {
+        if (v.isLeftValue) {
+            return v.sym
+        } else {
+            return this.getVariableValue(v.sym as VarSymbol)
+        }
+    }
+
+    private getVariableValue(sym: VarSymbol): any {
+        return this.currentFrame.values.get(sym)
+    }
+
+    private setVariableValue(sym: VarSymbol, value: any): any {
+        return this.currentFrame.values.set(sym, value)
+    }
 
     /**
      * 计算二元表达式
      * @param bi
      */
-    visitBinary(bi: Binary): any {}
+    visitBinary(bi: Binary): any {
+        let ret: any
+        let v1 = this.visit(bi.exp1)
+        let v2 = this.visit(bi.exp2)
+        switch (bi.op) {
+            case Op.Plus: //'+'
+                ret = v1 + v2
+                break
+            case Op.Minus: //'-'
+                ret = v1 - v2
+                break
+            case Op.Multiply: //'*'
+                ret = v1 * v2
+                break
+            case Op.Divide: //'/'
+                ret = v1 / v2
+                break
+            case Op.Modulus: //'%'
+                ret = v1 % v2
+                break
+            case Op.G: //'>'
+                ret = v1 > v2
+                break
+            case Op.GE: //'>='
+                ret = v1 >= v2
+                break
+            case Op.L: //'<'
+                ret = v1 < v2
+                break
+            case Op.LE: //'<='
+                ret = v1 <= v2
+                break
+            case Op.EQ: //'=='
+                ret = v1 == v2
+                break
+            case Op.NE: //'!='
+                ret = v1 != v2
+                break
+            case Op.And: //'&&'
+                ret = v1 && v2
+                break
+            case Op.Or: //'||'
+                ret = v1 || v2
+                break
+            case Op.Assign: //'='
+                let varSymbol = v1 as VarSymbol
+                this.setVariableValue(varSymbol, v2)
+
+                break
+            default:
+                console.log('Unsupported binary operation: ' + Op[bi.op])
+        }
+        return ret
+    }
 
     /**
      * 计算一元表达式
      * @param u
      */
-    visitUnary(u: Unary): any {}
+    visitUnary(u: Unary): any {
+        let v = this.visit(u.exp)
+        let varSymbol: VarSymbol
+        let value: any
+
+        switch (u.op) {
+            case Op.Inc:
+                varSymbol = v as VarSymbol
+                value = this.getVariableValue(varSymbol)
+                this.setVariableValue(varSymbol, value + 1)
+                if (u.isPrefix) {
+                    return value + 1
+                } else {
+                    return value
+                }
+
+                break
+            case Op.Dec: //'--'
+                varSymbol = v as VarSymbol
+                value = this.getVariableValue(varSymbol)
+                this.setVariableValue(varSymbol, value - 1)
+                if (u.isPrefix) {
+                    return value - 1
+                } else {
+                    return value
+                }
+                break
+
+            case Op.Plus: //'+'
+                return v //不需要做任何动作
+            case Op.Minus: //'-'
+                return -v //对值取反
+            default:
+                console.log('Unsupported unary op: ' + Op[u.op])
+        }
+    }
 }
 
 /**
@@ -139,8 +351,10 @@ export class Intepretor extends AstVisitor {
  * 每个函数对应一级栈帧
  */
 class StackFrame {
+    // 存储变量的值
     values: Map<Symbol, any> = new Map()
 
+    // 返回值，当函数调用的时候，返回值放在这里
     retVal: any = undefined
 }
 
