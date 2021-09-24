df32c8cff67ed990f4ff3dd3decce4ad2a0cde20
zhuzhenfeng.code <zhuzhenfeng.code@bytedance.com>
2021-09-24 22:20:34 +0800

    java字节码

---
+++ A.java
@@ -0,0 +1,9 @@
+//实现同样功能的Java程序。
+public class A{
+    public static int foo(int a){
+        return a + 10;
+    }
+    public static void main(String args[]){
+        System.out.println(foo(8));
+    }
+}
